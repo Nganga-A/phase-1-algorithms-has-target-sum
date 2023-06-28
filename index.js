0b5cf2f96a30097db9606e5c027a85053507cf41
Abed Nganga <ngangaabed@gmail.com>
2023-06-29 01:35:06 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  const complements = new Set(); //create an empty set to track complements
+
+  for (let num of array) {
+    const complement = target - num;
+    if (complements.has(complement)) {
+      return true;
+    }
+    complements.add(num);
+  }
+
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  creating a set and checking for complements takes constant time o(1)
+  o(n) where n is the length of input array
+  c
 */
 
 /* 
   Add your pseudocode here
+
+  Create an empty set to store complements
+  Iterate through each element in the array
+  Calculate the complement required to reach the target
+  Check if the complement exists in the set
+  if complements contains complement return true
+  add the current complement to the set of complements
+
 */
 
 /*
   Add written explanation of your solution here
+
+  our solution has a function that checks whether there exists a pair of elements in the array that sums up to the target value. 
 */
 
 // You can run `node index.js` to view these console logs
